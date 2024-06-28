e0364a344308d414ebe0eda6728529dea537dff8
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-28 20:44:13 +0000

    Add form validation

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// Form validation
-const form = document.querySelector(".needs-validation");
-form.addEventListener(
-	"submit",
-	(event) => {
-		if (!form.checkValidity()) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
+const bookForm = document.querySelector("form");
+const addBtn = document.querySelector("#add-button");
+const formSubmitBtn = bookForm.querySelector("#form-submit-btn");
 
-		form.classList.add("was-validated");
-	},
-	false
+bookForm.addEventListener(
+  "submit",
+  (event) => {
+    if (!bookForm.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    bookForm.classList.add("was-validated");
+  },
+  false
 );
+addBtn.addEventListener("click", () => formSubmitBtn.click());
