15762cec6fde9b83207c682e95d3e16af51e178b
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-30 08:04:43 +0000

    Format code with standard --fix

--- script.js
+++ script.js
@@ -1,173 +1,173 @@
 class Library {
-  constructor() {
-    this.books = [];
+  constructor () {
+    this.books = []
   }
 
-  addBook(book) {
-    this.books.push(book);
+  addBook (book) {
+    this.books.push(book)
   }
 
-  removeBook(index) {
-    this.books.splice(index, 1);
+  removeBook (index) {
+    this.books.splice(index, 1)
   }
 
-  toggleReadStatus(index) {
-    this.books[index].isRead = !this.books[index].isRead;
+  toggleReadStatus (index) {
+    this.books[index].isRead = !this.books[index].isRead
   }
 }
 
 class Book {
-  constructor(title, author, numOfPages, isRead = false) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.isRead = isRead;
+  constructor (title, author, numOfPages, isRead = false) {
+    this.title = title
+    this.author = author
+    this.numOfPages = numOfPages
+    this.isRead = isRead
   }
 }
 
 class BookView {
-  constructor() {
-    this.bookModal = document.querySelector("#book-modal");
-    this.bookForm = document.querySelector("form");
-    this.formSubmitBtn = document.querySelector("#form-submit-btn");
-    this.booksContainer = document.querySelector("#books-container");
-    this.closeModalBtns = document.querySelectorAll(".close-modal");
+  constructor () {
+    this.bookModal = document.querySelector('#book-modal')
+    this.bookForm = document.querySelector('form')
+    this.formSubmitBtn = document.querySelector('#form-submit-btn')
+    this.booksContainer = document.querySelector('#books-container')
+    this.closeModalBtns = document.querySelectorAll('.close-modal')
 
     // Add event listener to focus on the first input field when the modal is shown
-    this.bookModal.addEventListener("shown.bs.modal", () => {
-      this.bookForm.querySelector("input").focus();
-    });
+    this.bookModal.addEventListener('shown.bs.modal', () => {
+      this.bookForm.querySelector('input').focus()
+    })
 
     // Add event listeners to clear form when book addition is cancelled
     this.closeModalBtns.forEach((closeModalBtn) => {
-      closeModalBtn.addEventListener("click", () => {
-        const form = closeModalBtn.closest(".modal").querySelector("form");
+      closeModalBtn.addEventListener('click', () => {
+        const form = closeModalBtn.closest('.modal').querySelector('form')
 
         if (form) {
-          form.reset();
-          form.classList.remove("was-validated");
+          form.reset()
+          form.classList.remove('was-validated')
         }
-      });
-    });
+      })
+    })
   }
 
-  bindAddBook(handler) {
+  bindAddBook (handler) {
     this.bookForm.addEventListener(
-      "submit",
+      'submit',
       (event) => {
-        event.preventDefault();
+        event.preventDefault()
         if (this.bookForm.checkValidity()) {
-          const title = this.bookForm.querySelector("#title").value;
-          const author = this.bookForm.querySelector("#author").value;
-          const numOfPages = this.bookForm.querySelector("#num-of-pages").value;
-          const isRead = this.bookForm.querySelector("#is-read").checked;
+          const title = this.bookForm.querySelector('#title').value
+          const author = this.bookForm.querySelector('#author').value
+          const numOfPages = this.bookForm.querySelector('#num-of-pages').value
+          const isRead = this.bookForm.querySelector('#is-read').checked
 
-          handler({ title, author, numOfPages, isRead });
+          handler({ title, author, numOfPages, isRead })
 
-          this.bookForm.reset();
-          this.bookForm.classList.remove("was-validated");
+          this.bookForm.reset()
+          this.bookForm.classList.remove('was-validated')
         } else {
-          event.stopPropagation();
-          this.bookForm.classList.add("was-validated");
+          event.stopPropagation()
+          this.bookForm.classList.add('was-validated')
         }
       },
       false
-    );
+    )
   }
 
-  hideModal() {
-    const modalInstance = bootstrap.Modal.getInstance(this.bookModal);
+  hideModal () {
+    const modalInstance = bootstrap.Modal.getInstance(this.bookModal)
     if (modalInstance) {
-      modalInstance.hide();
+      modalInstance.hide()
     }
   }
 
-  displayBooks(books, handlers) {
-    this.booksContainer.innerHTML = "";
+  displayBooks (books, handlers) {
+    this.booksContainer.innerHTML = ''
     books.forEach((book, index) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
+      const card = document.createElement('div')
+      card.classList.add('card')
 
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
+      const cardBody = document.createElement('div')
+      cardBody.classList.add('card-body')
 
-      const cardTitle = document.createElement("h5");
-      cardTitle.classList.add("card-title", "fw-bold");
-      cardTitle.textContent = book.title;
-      cardBody.appendChild(cardTitle);
+      const cardTitle = document.createElement('h5')
+      cardTitle.classList.add('card-title', 'fw-bold')
+      cardTitle.textContent = book.title
+      cardBody.appendChild(cardTitle)
 
-      const cardAuthor = document.createElement("p");
-      cardAuthor.classList.add("card-text", "mb-0");
-      cardAuthor.textContent = `Author: ${book.author}`;
-      cardBody.appendChild(cardAuthor);
+      const cardAuthor = document.createElement('p')
+      cardAuthor.classList.add('card-text', 'mb-0')
+      cardAuthor.textContent = `Author: ${book.author}`
+      cardBody.appendChild(cardAuthor)
 
-      const cardPages = document.createElement("p");
-      cardPages.classList.add("card-text", "mb-0");
-      cardPages.textContent = `Pages: ${book.numOfPages}`;
-      cardBody.appendChild(cardPages);
+      const cardPages = document.createElement('p')
+      cardPages.classList.add('card-text', 'mb-0')
+      cardPages.textContent = `Pages: ${book.numOfPages}`
+      cardBody.appendChild(cardPages)
 
-      const cardReadStatus = document.createElement("p");
-      cardReadStatus.classList.add("card-text", "mb-2");
-      cardReadStatus.textContent = book.isRead ? "Read" : "Not Read";
-      cardBody.appendChild(cardReadStatus);
+      const cardReadStatus = document.createElement('p')
+      cardReadStatus.classList.add('card-text', 'mb-2')
+      cardReadStatus.textContent = book.isRead ? 'Read' : 'Not Read'
+      cardBody.appendChild(cardReadStatus)
 
-      const toggleReadBtn = document.createElement("button");
-      toggleReadBtn.classList.add("btn", "btn-primary", "me-2");
+      const toggleReadBtn = document.createElement('button')
+      toggleReadBtn.classList.add('btn', 'btn-primary', 'me-2')
       toggleReadBtn.textContent = book.isRead
-        ? "Mark as Unread"
-        : "Mark as Read";
-      toggleReadBtn.addEventListener("click", () => handlers.toggleRead(index));
-      cardBody.appendChild(toggleReadBtn);
+        ? 'Mark as Unread'
+        : 'Mark as Read'
+      toggleReadBtn.addEventListener('click', () => handlers.toggleRead(index))
+      cardBody.appendChild(toggleReadBtn)
 
-      const deleteBtn = document.createElement("button");
-      deleteBtn.classList.add("btn", "btn-danger");
-      deleteBtn.textContent = "Delete";
-      deleteBtn.addEventListener("click", () => handlers.delete(index));
-      cardBody.appendChild(deleteBtn);
+      const deleteBtn = document.createElement('button')
+      deleteBtn.classList.add('btn', 'btn-danger')
+      deleteBtn.textContent = 'Delete'
+      deleteBtn.addEventListener('click', () => handlers.delete(index))
+      cardBody.appendChild(deleteBtn)
 
-      card.appendChild(cardBody);
-      this.booksContainer.appendChild(card);
-    });
+      card.appendChild(cardBody)
+      this.booksContainer.appendChild(card)
+    })
   }
 }
 
 class BookController {
-  constructor(model, view) {
-    this.model = model;
-    this.view = view;
+  constructor (model, view) {
+    this.model = model
+    this.view = view
 
-    this.view.bindAddBook(this.handleAddBook.bind(this));
-    this.displayBooks();
+    this.view.bindAddBook(this.handleAddBook.bind(this))
+    this.displayBooks()
   }
 
-  handleAddBook(bookDetails) {
+  handleAddBook (bookDetails) {
     const book = new Book(
       bookDetails.title,
       bookDetails.author,
       bookDetails.numOfPages,
       bookDetails.isRead
-    );
-    this.model.addBook(book);
-    this.view.hideModal();
-    this.displayBooks();
+    )
+    this.model.addBook(book)
+    this.view.hideModal()
+    this.displayBooks()
   }
 
-  handleToggleReadStatus(index) {
-    this.model.toggleReadStatus(index);
-    this.displayBooks();
+  handleToggleReadStatus (index) {
+    this.model.toggleReadStatus(index)
+    this.displayBooks()
   }
 
-  handleDeleteBook(index) {
-    this.model.removeBook(index);
-    this.displayBooks();
+  handleDeleteBook (index) {
+    this.model.removeBook(index)
+    this.displayBooks()
   }
 
-  displayBooks() {
+  displayBooks () {
     this.view.displayBooks(this.model.books, {
       toggleRead: this.handleToggleReadStatus.bind(this),
-      delete: this.handleDeleteBook.bind(this),
-    });
+      delete: this.handleDeleteBook.bind(this)
+    })
   }
 }
 
-const app = new BookController(new Library(), new BookView());
+const app = new BookController(new Library(), new BookView())
